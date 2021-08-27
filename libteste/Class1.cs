1d81133ab67c0bff53452aef31ebf671957f1197
Leandro Damascena <leandro.damascena@gmail.com>
2021-08-27 21:21:49 +0100

    Libteste

---
+++ libteste/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace libteste
+{
+    public class libteste
+    {
+        public void Teste(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
